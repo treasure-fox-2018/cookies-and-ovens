d67492e31803f69bc39bcd4a9b8c4b4968444bbe
Andre Sudi Yudatama <andresudi@Andres-MacBook-Pro.local>
2018-06-06 01:52:42 +0700

    kue n oven kelar

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,60 @@
 //
 //
 // Your code here
+
+class Cake {
+    constructor() {
+        this.bakedTimer = 30
+    }
+
+    panggang() {
+        for (let i = 0; i < this.bakedTimer+5; i+= 5) {
+            if(i === 5) {
+                console.log(`${this.cakeName}, menit ke ${i} : mentah`)
+            } else if (i === 10) {
+                console.log(`${this.cakeName}, menit ke ${i} : hampir mentah`)
+            } else if (i === 15) {
+                console.log(`${this.cakeName}, menit ke ${i} : hampir metang`)
+            } else if (i === 20) {
+                console.log(`${this.cakeName}, menit ke ${i} : matang`)
+            } else if (i > 25) {
+                console.log(`${this.cakeName}, menit ke ${i} : hangus`)
+            }
+        }
+    }
+}
+
+class cokelat extends Cake {
+    constructor() {
+        super()
+        this.bakedTimer = 20
+        this.cakeName = 'Kue Cokelat'
+    }
+}
+
+class kacang extends Cake {
+    constructor() {
+        super()
+        this.bakedTimer = 30
+        this.cakeName = 'Kue Kacang'
+    }
+}
+
+class keju extends Cake {
+    constructor() {
+        super()
+        this.bakedTimer = 35
+        this.cakeName = 'Kue Keju'
+    }
+}
+
+var cookies_and_ovens = new cokelat()
+var cookies_and_ovens1 = new kacang()
+var cookies_and_ovens2 = new keju()
+
+cookies_and_ovens.panggang()
+cookies_and_ovens1.panggang()
+cookies_and_ovens2.panggang()
+
+
+
