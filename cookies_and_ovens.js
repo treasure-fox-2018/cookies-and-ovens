1294c7a09903c865e7825eec87094c181349d77e
Ari Supriatna <arisupriatna@MacBook-Air-Ari.local>
2018-06-05 23:52:36 +0700

    challenge done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -9,24 +9,78 @@
 //
 // Your code here
 
-class Oven {
-  constructor (time) {
-    this._time = time
+class Cookies {
+  constructor (nameCake, times) {
+    this._nameCake = nameCake
+    this._timesCooking = times
+  }
+
+  get namesCake () {
+    return this._nameCake
   }
 
   get time() {
-    
+    return this._timesCooking
   }
 }
 
-class Coklat extends Oven {
+class Coklat extends Cookies {
+  constructor () {
+    super('Choco Cake', 20)
+  }
+}
+
+class Kacang extends Cookies {
+  constructor () {
+    super ('Peanuts Cake', 30)
+  }
 
 }
 
-class Kacang extends Oven {
-
+class Keju extends Cookies {
+  constructor () {
+    super ('Cheese Cake', 35)
+  }
 }
 
-class Keju extends Oven {
+class Oven {
+  constructor (listCakes) {
+    this._myCake = listCakes
+  }
 
+  baking(timeOven) {
+    let listCake = this._myCake
+
+    for (let i = 0; i <= timeOven; i+=5) {
+      if (i === listCake._timesCooking) {
+        console.log(`${listCake._nameCake} menit ke - ${i} : Kue sudah matang`)
+      } else if (i === listCake._timesCooking - 5) {
+        console.log(`${listCake._nameCake} menit ke - ${i} : Kue setengah matang`)
+      } else if (i > listCake._timesCooking) {
+        console.log(`${listCake._nameCake} menit ke - ${i} : Kue gosong`)
+      } else {
+        console.log(`${listCake._nameCake} menit ke - ${i} : Kue belum matang`)
+      }
+    }
+  }
 }
+
+
+const cakeCoklat = new Coklat()
+const cakePeanut = new Kacang()
+const cakeCheese = new Keju()
+
+console.log('         ====> Kue Coklat <====         ')
+const ovenCoklat = new Oven (cakeCoklat)
+ovenCoklat.baking(25)
+console.log(ovenCoklat)
+
+console.log('         ====> Kue Kacang <====         ')
+const ovenPeanut = new Oven (cakePeanut)
+ovenPeanut.baking(30)
+console.log(ovenPeanut)
+
+console.log('         ====> Kue Keju <====         ')
+const ovenCheese = new Oven (cakeCheese)
+ovenCheese.baking(35)
+console.log(ovenCheese)
