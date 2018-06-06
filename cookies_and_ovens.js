9d4e09a8eedbaec20dedd2b52e6809bcfd3424b5
ronifajrian <ronifajrian@gmail.com>
2018-06-06 23:31:42 +0700

    add cookies and ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,77 @@
 //
 //
 // Your code here
+
+class Cake{
+    constructor(jenis,waktuMatang){
+        this._jenis = jenis
+        this._waktuMatang = waktuMatang
+    }
+
+    get jenis(){
+        return this._jenis
+    }
+
+    get waktuMatang(){
+        return this._waktuMatang
+    }
+
+    cook(time){
+        //5,10,15,20,25 [mentah,mentah,hampir matang,matang,hangus]
+        // var statusWaktu = [5,10,15,20,25]
+        var statusMatang
+        for (let i = 0; i <= time; i++) {
+            if (i % 5 == 0) {
+                this.time = i
+                if (this.time <= 10) {
+                    statusMatang = 'mentah'
+                }else
+                if (this.time < this.waktuMatang) {
+                    statusMatang = 'hampir matang'
+                }else
+                if (this.time == this.waktuMatang) {
+                    statusMatang = 'MATANG'
+                }else
+                if (this.time > this.waktuMatang) {
+                    statusMatang = 'hangus'
+                }
+
+                console.log(`${this.jenis}, menit ke ${i} : ${statusMatang}`);
+                
+            }
+        }
+    }
+
+
+}
+
+class Coklat extends Cake{
+    constructor(){
+        super('coklat',20)
+    }
+}
+
+class Kacang extends Cake{
+    constructor(){
+        super('kacang',30)
+    }
+}
+
+class Keju extends Cake{
+    constructor(){
+        super('keju',35)
+    }
+}
+
+var coklat = new Coklat()
+var kacang = new Kacang()
+var keju = new Keju()
+
+coklat.cook(30)
+console.log(coklat);
+
+kacang.cook(40)
+console.log(kacang);
+
+keju.cook(50)
+console.log(keju);
